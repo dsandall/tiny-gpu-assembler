fd3c76fbf1ab0ad228b7deba0b3979032e04a71b
d <dsandall@calpoly.edu>
2025-05-27 19:54:21 -0700

    updated tests

--- asm_src/test_alldmem_unrolled.asm
+++ asm_src/test_alldmem_unrolled.asm
@@ -3,10 +3,10 @@
 ;
 ; scored 2662 cycles
 ;
-.threads 4
+.threads 8
 ;CONST R5, #1 ; #threads
 
-CONST R1, #16 ; #spread
+CONST R1, #8 ; #spread
 MUL R0, %blockIdx, %blockDim    ; Compute global thread index
 ADD R0, R0, %threadIdx          ; R0 = thread ID
 MUL R0, R0, R1 ; apply spread
@@ -17,22 +17,22 @@ LOOP:
   CONST R1, #1 ;
   ADD R2, R2, R1 ; inc tracker
 
-  STR R0, R0 ; store current
-  ADD R0, R0, R1 ; inc addr
-  STR R0, R0 ; store current; 8x unrolled
-  ADD R0, R0, R1 ; inc addr ; 8x unrolled
-  STR R0, R0 ; store current
-  ADD R0, R0, R1 ; inc addr
-  STR R0, R0 ; store current; 8x unrolled
-  ADD R0, R0, R1 ; inc addr ; 8x unrolled
-  STR R0, R0 ; store current
-  ADD R0, R0, R1 ; inc addr
-  STR R0, R0 ; store current; 8x unrolled
-  ADD R0, R0, R1 ; inc addr ; 8x unrolled
-  STR R0, R0 ; store current
-  ADD R0, R0, R1 ; inc addr
-  STR R0, R0 ; store current; 8x unrolled
-  ADD R0, R0, R1 ; inc addr ; 8x unrolled
+;  STR R0, R0 ; store current
+;  ADD R0, R0, R1 ; inc addr
+;  STR R0, R0 ; store current; 8x unrolled
+;  ADD R0, R0, R1 ; inc addr ; 8x unrolled
+;  STR R0, R0 ; store current
+;  ADD R0, R0, R1 ; inc addr
+;  STR R0, R0 ; store current; 8x unrolled
+;  ADD R0, R0, R1 ; inc addr ; 8x unrolled
+;  STR R0, R0 ; store current
+;  ADD R0, R0, R1 ; inc addr
+;  STR R0, R0 ; store current; 8x unrolled
+;  ADD R0, R0, R1 ; inc addr ; 8x unrolled
+;  STR R0, R0 ; store current
+;  ADD R0, R0, R1 ; inc addr
+;  STR R0, R0 ; store current; 8x unrolled
+;  ADD R0, R0, R1 ; inc addr ; 8x unrolled
   STR R0, R0 ; store current
   ADD R0, R0, R1 ; inc addr
   STR R0, R0 ; store current; 8x unrolled
@@ -50,7 +50,8 @@ LOOP:
   STR R0, R0 ; store current; 8x unrolled
   ADD R0, R0, R1 ; inc addr ; 8x unrolled
 
-  CONST R1, #48 ; 16*(4-1)
+  ;CONST R1, #48 ; 16*(4-1)
+  CONST R1, #56 ; 8*(8-1)
   ADD R0, R0, R1 ;
 
   CONST R1, #4  ; #times to loop
--- asm_src/test_matadd_32_threads.asm
+++ asm_src/test_matadd_32_threads.asm
@@ -1,6 +1,15 @@
-.threads 8
-.data 1 2 3 4 5 6 7 8        ; matrix A (1 x 8)
-.data 11 12 13 14 15 16 17 18         ; matrix B (1 x 8)
+.threads 32
+; matrix A (1 x 32)
+.data 1 2 3 4 5 6 7 8
+.data 11 12 13 14 15 16 17 18
+.data 21 22 23 24 25 26 27 28
+.data 31 32 33 34 35 36 37 38
+; matrix B (1 x 32)
+.data 41 42 43 44 45 46 47 48
+.data 51 52 53 54 55 56 57 58
+.data 61 62 63 64 65 66 67 68
+.data 71 72 73 74 75 76 77 78
+; matrix C (1 x 32):
                                ;@(reset):
                                ;registers[13] <= block_id;          // %blockIdx
                                ;registers[14] <= THREADS_PER_BLOCK; // %blockDim
@@ -9,8 +18,8 @@ MUL R0, %blockIdx, %blockDim
 ADD R0, R0, %threadIdx         ; i = blockIdx * blockDim + threadIdx
 
 CONST R1, #0                   ; baseA (matrix A base address)
-CONST R2, #8                   ; baseB (matrix B base address)
-CONST R3, #16                  ; baseC (matrix C base address)
+CONST R2, #32                  ; baseB (matrix B base address)
+CONST R3, #64                  ; baseC (matrix C base address)
 
 ADD R4, R1, R0                 ; addr(A[i]) = baseA + i
 LDR R4, R4                     ; load A[i] from global memory
